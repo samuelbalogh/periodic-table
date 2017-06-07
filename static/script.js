d4755f1f185afdb8a0a5186b87b1975be50b96c1
MaxMontoro <samubalogh@gmail.com>
2017-06-07 22:55:53 +0200

    style: add highlight to categories

--- static/script.js
+++ static/script.js
@@ -13,3 +13,22 @@ window.addEventListener('keydown', function(e) {
         close_modals();
     };
 });
+
+
+highlight_category = function(category) {
+  var category_parts = category.split(' ');
+  category = category_parts.join('-');
+  var elements = document.getElementsByClassName(category);
+  Array.from(elements).forEach(function(element) {
+    element.style.boxShadow = '0 0 10px rgb(30, 111, 185)';
+  });
+};
+
+de_highlight_category = function(category) {
+  var category_parts = category.split(' ');
+  category = category_parts.join('-');
+  var elements = document.getElementsByClassName(category);
+  Array.from(elements).forEach(function(element) {
+    element.style.boxShadow = 'none';
+  });
+};
